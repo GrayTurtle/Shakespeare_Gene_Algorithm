b36f535a0c5f7d4fd6475768c523a5641616b169
GrayTurtle <John.Tran@ou.edu>
2016-12-15 22:22:45 -0600

    create fitness

--- DNA.java
+++ DNA.java
@@ -1,12 +1,26 @@
+import java.lang.Math;
 class DNA {
-
+    String target;
+    float fitness;
     //random phrase
     char[] genes = new char[18];
 
     //create random phrase
-    DNA() {
+    DNA(String tar) {
+        target = tar;
         for (int i = 0; i < genes.length; i++) {
-            genes[i] = (char) random(32,128);
+            genes[i] = (char) ((Math.random() * (128-32)) + 32) ;
         }
+    }
+
+    //calulate fitness
+    void fitness() {
+        int score = 0;
+        for (int i = 0; i < genes.length; i++) {
+            if (genes[i] == target.charAt(i)) {
+                score++;
+            }
+        }
+        fitness = (float)score/target.length();
     }
 }
--- ShakespeareGA.java
+++ ShakespeareGA.java
@@ -1,11 +1,23 @@
+//a project tutorial that
+//I made from http://natureofcode.com/book/chapter-9-the-evolution-of-code/
+//Adapted to Java
+
 class ShakespeareGA {
 	//population of 100
 	DNA[] population = new DNA[100];
 
 	void setup() {
+		String target = "to be or not to be";
+
 		//initialize each member of population
 		for (int i = 0; i < population.length; i++) {
-			population[i] = new DNA();
+			population[i] = new DNA(target);
 	  }
+	}
+
+	void draw() {
+		for (int i = 0; i < population.length; i++) {
+			population[i].fitness();
+		}
 	}
 }
